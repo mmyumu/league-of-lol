1f6a3e40f0631f7d86f3ff1dd89342e0aad4f154
mmyumu <mmyumu@cee9527c-27da-ebe5-6d4b-6d5179be0fa7>
2013-12-20 00:00:31 +0000

    test ajax

--- lolol/src/Lolol/TeamBundle/Controller/DefaultController.php
+++ lolol/src/Lolol/TeamBundle/Controller/DefaultController.php
@@ -3,17 +3,19 @@
 namespace Lolol\TeamBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller {
 	public function indexAction() {
 		// Créons nous-mêmes la réponse en JSON, grâce à la fonction json_encode()
-		$response = new Response( json_encode( array ("code" => 100,"success" => true 
+		$response = new Response ( json_encode ( array (
+				"code" => 100,
+				"success" => true 
 		) ) );
 		
 		// Ici, nous définissons le Content-type pour dire que l'on renvoie du JSON et non du HTML
-		$response->headers->set( 'Content-Type', 'application/json' );
+		$response->headers->set ( 'Content-Type', 'application/json' );
 		
 		return $response;
-		// return $this->render( 'LololTeamBundle:Default:index.html.twig' );
 	}
 }
