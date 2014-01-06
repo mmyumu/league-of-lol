fbe209a676055d8ca2f23afa8e0131d88fff8650
mmyumu <mmyumu@cee9527c-27da-ebe5-6d4b-6d5179be0fa7>
2014-01-06 00:56:15 +0000

    Retrieve champions icons (48px) added Retrieve champions updated

--- lolol/src/Lolol/ShopBundle/Controller/ShopController.php
+++ lolol/src/Lolol/ShopBundle/Controller/ShopController.php
@@ -4,10 +4,20 @@ namespace Lolol\ShopBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class ShopController extends Controller
-{
-    public function indexAction()
-    {
-        return $this->render('LololShopBundle:Shop:index.html.twig');
-    }
+class ShopController extends Controller {
+	public function indexAction() {
+		return $this->render('LololShopBundle:Shop:index.html.twig');
+	}
+	public function championsAction() {
+		$em = $this->getDoctrine()->getManager();
+		
+		$champions = $em->getRepository('LololTeamBundle:Champion')->findAll();
+		
+		return $this->render('LololShopBundle:Shop:champions.html.twig', array(
+				'champions' => $champions
+		));
+	}
+	public function itemsAction() {
+		return $this->render('LololShopBundle:Shop:items.html.twig');
+	}
 }
