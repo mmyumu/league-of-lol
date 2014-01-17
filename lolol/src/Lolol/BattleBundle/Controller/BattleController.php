c0e1febb815168ead4e297eaaeb7c0b777e68409
mmyumu@gmail.com <mmyumu@gmail.com@cee9527c-27da-ebe5-6d4b-6d5179be0fa7>
2014-01-17 15:44:27 +0000

    New bundle Battle

---
+++ lolol/src/Lolol/BattleBundle/Controller/BattleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Lolol\BattleBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BattleController extends Controller {
+	public function indexAction() {
+		return $this->render('LololBattleBundle:Battle:index.html.twig');
+	}
+	
+	public function attackAction() {
+		return $this->render('LololBattleBundle:Battle:attack.html.twig');
+	}
+	
+	public function defenseAction() {
+		return $this->render('LololBattleBundle:Battle:defense.html.twig');
+	}
+}
