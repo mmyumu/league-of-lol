ea772ebb90834da3db11c229025387ed9eacae74
mmyumu@gmail.com <mmyumu@gmail.com@cee9527c-27da-ebe5-6d4b-6d5179be0fa7>
2014-01-27 17:58:30 +0000

    Initialize attack page

--- lolol/src/Lolol/BattleBundle/Controller/BattleController.php
+++ lolol/src/Lolol/BattleBundle/Controller/BattleController.php
@@ -9,9 +9,51 @@ class BattleController extends Controller {
 	public function indexAction() {
 		return $this->render('LololBattleBundle:Battle:index.html.twig');
 	}
+	
+	/**
+	 *	Display attack page 
+	 */
 	public function attackAction() {
-		return $this->render('LololBattleBundle:Battle:attack.html.twig');
+		// Get the parameters
+		$folder = $this->container->getParameter('champions_folder');
+		$prefixIcons20 = $this->container->getParameter('champions_icons20_prefix');
+		$suffixIcons = $this->container->getParameter('champions_icons_suffix');
+		$teamSize = $this->container->getParameter('team_size');
+		
+		// Get teams
+		$em = $this->getDoctrine()->getManager();
+		$teams = $em->getRepository('LololTeamBundle:Team')->findByUser($this->getUser());
+		$championRepo = $em->getRepository('LololTeamBundle:ChampionTeam');
+		
+		// Get champions by teams
+		$i = 0;
+		$result = array();
+		$defender = array();
+		foreach($teams as $team) {
+			$champions = $championRepo->getWithChampionsByTeam($team, 'ct.position');
+			$result[$i]['team'] = $team;
+			$result[$i]['champions'] = $champions;
+				
+			// Initialize the defender team
+			if ($team->isDefender()) {
+				$defender['team'] = $team;
+				$defender['champions'] = $champions;
+			}
+			$i ++;
+		}
+		
+		return $this->render('LololBattleBundle:Battle:attack.html.twig', array(
+				'results' => $result,
+				'defender' => $defender,
+				'folder' => $folder,
+				'prefixIcons20' => $prefixIcons20,
+				'suffixIcons' => $suffixIcons,
+				'teamSize' => $teamSize));
 	}
+	
+	/**
+	 *	Display defense page
+	 */
 	public function defenseAction() {
 		// Get the parameters
 		$folder = $this->container->getParameter('champions_folder');
